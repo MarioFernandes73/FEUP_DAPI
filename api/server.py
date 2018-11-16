1fa202a30c1bfa9be8e11840ca399f8d3a72b6f6
Mário Fernandes <mgustavofernandes@gmail.com>
2018-11-16 14:19:55 +0000

    es index

--- api/server.py
+++ api/server.py
@@ -3,6 +3,8 @@ from elasticsearch import Elasticsearch
 import json
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
+# es.index(index='sw', doc_type='people', id=i, body=json.loads(r.content)
+
 # x = es.search(index="sw", body={"query": {"match": {'name':'Darth Vader'}}})
 # print(x)
 
